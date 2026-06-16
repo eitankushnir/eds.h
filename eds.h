01546f4cc0494d4e43095cf126a2120804624a99
eitankushnir <etn121415@gmail.com>
2026-06-16 22:40:23 +0300

    WIP. hashmap - insert, grow, search

--- eds.h
+++ eds.h
@@ -101,15 +101,45 @@ void _list_insert(list_t *list, size_t index, void *data);
 void list_remove(list_t *list, size_t index);
 void list_pop(list_t *list, size_t index, void *out_target);
 
-list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *), char *type_name);
+list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *), const char *type_name);
 void _list_destroy(list_t **list);
 
 void _list_assert_type(list_t *list, char *check, char *action);
+
 #endif // EDS_NO_LIST
 
 #ifndef EDS_NO_HASHMAP
 
 typedef struct hashmap hashmap_t;
+#define EDS_HASHMAP_INITIAL_CAPACITY 8
+#define EDS_HASHMAP_LOAD_FACTOR 0.6f
+
+#define hashmap(key, val) _hashmap_create(EDS_HASHMAP_INITIAL_CAPACITY, sizeof(key), sizeof(val), #key, #val, _eds_cmp_bytes, _eds_hash_bytes, EDS_HASHMAP_LOAD_FACTOR)
+#define strmap(val) _hashmap_create(EDS_HASHMAP_INITIAL_CAPACITY, sizeof(char *), sizeof(val), "strmap", #val, _eds_cmp_str, _eds_hash_str, EDS_HASHMAP_LOAD_FACTOR)
+#define hashmap_destroy(hashmap) _hashmap_destroy(&(hashmap))
+
+hashmap_t *_hashmap_create(
+    size_t capacity,
+    size_t key_size,
+    size_t val_size,
+    char *key_name,
+    char *val_name,
+    bool (*cmp_fn)(const void *, const void *, size_t),
+    size_t (*hash_fn)(const void *, size_t),
+    float load_factor);
+
+void hashmap_set_key_free_fn(hashmap_t *hashmap, void (*key_free_fn)(void *));
+void hashmap_set_val_free_fn(hashmap_t *hashmap, void (*val_free_fn)(void *));
+
+void _hashmap_destroy(hashmap_t **hashmap);
+
+size_t hashmap_capacity(hashmap_t *hashmap);
+size_t hashmap_size(hashmap_t *hashmap);
+
+void _hashmap_grow(hashmap_t *hashmap);
+void _hashmap_insert(hashmap_t *hashmap, void *key, void *value);
+
+bool _hashmap_get(hashmap_t *hashmap, void *key, void *out_target);
 
 #endif // EDS_NO_HASHMAP
 
@@ -162,7 +192,7 @@ struct list {
   void (*free_fn)(void *);
 };
 
-list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *), char *type_name) {
+list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *), const char *type_name) {
   if (!capacity) {
     eds_error("list_t cannot have 0 capacity");
   }
@@ -336,29 +366,203 @@ void list_set_capacity(list_t *list, size_t capacity) {
 bool _list_of_type(list_t *l, char *type_name) {
   return strcmp(l->type_name, type_name) == 0;
 }
-
 #endif // EDS_NO_LIST
 
 #ifndef EDS_NO_HASHMAP
 
+size_t _eds_hash_bytes(const void *key, size_t key_size) {
+  // FNV-1a hashing
+  size_t hash = 14695981039346656037ULL; // FNV-1 64-bit offset basis.
+
+  const char *bytes = (const char *)key;
+  for (size_t i = 0; i < key_size; i++) {
+    hash = (hash ^ bytes[i]);
+    hash = hash * 1099511628211; // FNV-1 prime.
+  }
+
+  return hash;
+}
+
+size_t _eds_hash_str(const void *key, size_t key_size) {
+  (void)key_size;
+  const char *str = *(const char **)key;
+  size_t len = strlen(str);
+
+  return _eds_hash_bytes(str, len);
+}
+
+bool _eds_cmp_str(const void *key1, const void *key2, const size_t key_size) {
+  (void)key_size;
+  char *str1 = *(char **)key1;
+  char *str2 = *(char **)key2;
+
+  return strcmp(str1, str2) == 0;
+}
+
+bool _eds_cmp_bytes(const void *key1, const void *key2, const size_t key_size) {
+  return memcmp(key1, key2, key_size) == 0;
+}
+
 struct hashmap {
-  list_t *data; // Stores Key-Value Pairs contigous in memory
-                // Each element is of size key_size + val_size
-                // They first key_size bytes are the key
-                // The next val_size bytes are the bytes
-
-  size_t key_size, val_size;
-  const char *key_name, val_name;
-
+  void *keys;
+  size_t key_size;
   void (*key_free_fn)(void *);
-  void (*val_free_fn)(void *);
+  const char *key_name;
 
-  bool is_string_key;
+  void *values;
+  size_t value_size;
+  void (*val_free_fn)(void *);
+  const char *val_name;
+
+  char *states;
+  size_t capacity;
+  size_t size;
+
+  bool (*cmp_fn)(const void *, const void *, size_t key_size);
+  size_t (*hash_fn)(const void *, size_t key_size);
+
+  float load_factor;
 };
 
-void _hashmap_create(size_t capacity, size_t key_size, size_t val_size);
-void hashmap_set_key_free_fn(void (*key_free_fn)(void *));
-void hashmap_set_val_free_fn(void (*val_free_fn)(void *));
+hashmap_t *_hashmap_create(
+    size_t capacity,
+    size_t key_size,
+    size_t val_size,
+    char *key_name,
+    char *val_name,
+    bool (*cmp_fn)(const void *, const void *, size_t),
+    size_t (*hash_fn)(const void *, size_t),
+    float load_factor) {
+
+  hashmap_t *hm = eds_malloc(sizeof(hashmap_t));
+  hm->keys = eds_malloc(key_size * capacity);
+  hm->key_size = key_size;
+  hm->key_free_fn = NULL;
+  hm->key_name = key_name;
+
+  hm->values = eds_malloc(val_size * capacity);
+  hm->value_size = val_size;
+  hm->val_free_fn = NULL;
+  hm->val_name = val_name;
+
+  hm->states = eds_malloc(sizeof(char) * capacity);
+  memset(hm->states, 0, sizeof(char) * capacity);
+
+  hm->capacity = capacity;
+  hm->size = 0;
+
+  hm->hash_fn = hash_fn;
+  hm->cmp_fn = cmp_fn;
+  hm->load_factor = load_factor;
+
+  return hm;
+}
+
+void hashmap_set_key_free_fn(hashmap_t *hashmap, void (*key_free_fn)(void *)) {
+  hashmap->key_free_fn = key_free_fn;
+}
+void hashmap_set_val_free_fn(hashmap_t *hashmap, void (*val_free_fn)(void *)) {
+  hashmap->val_free_fn = val_free_fn;
+}
+
+void _hashmap_destroy(hashmap_t **hashmap) {
+  hashmap_t *hm = *hashmap;
+  if (!hm)
+    return;
+
+  if (hm->val_free_fn) {
+    for (size_t i = 0; i < hm->capacity; i++) {
+      if (hm->states[i] == 1)
+        hm->val_free_fn((char *)hm->values + i * hm->value_size);
+    }
+  }
+
+  if (hm->key_free_fn) {
+    for (size_t i = 0; i < hm->capacity; i++) {
+      if (hm->states[i] == 1)
+        hm->key_free_fn((char *)hm->keys + i * hm->key_size);
+    }
+  }
+
+  free(hm->values);
+  free(hm->keys);
+  free(hm->states);
+  free(hm);
+  *hashmap = NULL;
+}
+
+size_t hashmap_capacity(hashmap_t *hashmap) {
+  return hashmap->capacity;
+}
+size_t hashmap_size(hashmap_t *hashmap) {
+  return hashmap->size;
+}
+
+void _hashmap_grow(hashmap_t *hashmap) {
+  size_t cap = hashmap->capacity;
+  size_t new_cap = hashmap->capacity * 2;
+
+  void *keys_grow = eds_malloc(hashmap->key_size * new_cap);
+  void *values_grow = eds_malloc(hashmap->value_size * new_cap);
+  char *states_grow = eds_malloc(sizeof(char) * new_cap);
+  memset(states_grow, 0, sizeof(char) * new_cap);
+
+  for (size_t i = 0; i < cap; i++) {
+    void *key = (char *)hashmap->keys + i * hashmap->key_size;
+    void *val = (char *)hashmap->values + i * hashmap->value_size;
+
+    size_t idx = hashmap->hash_fn(key, hashmap->key_size) & (hashmap->capacity - 1);
+    for (size_t i = idx; i < new_cap; i++) {
+      if (states_grow[i] == 0) {
+        memcpy((char *)keys_grow + i * hashmap->key_size, key, hashmap->key_size);
+        memcpy((char *)values_grow + i * hashmap->value_size, val, hashmap->value_size);
+        states_grow[i] = 1;
+      }
+    }
+  }
+
+  free(hashmap->keys);
+  free(hashmap->values);
+  hashmap->keys = keys_grow;
+  hashmap->values = values_grow;
+}
+
+void _hashmap_insert(hashmap_t *hashmap, void *key, void *value) {
+  if (hashmap->size >= hashmap->capacity * hashmap->load_factor) {
+    _hashmap_grow(hashmap);
+  }
+
+  size_t idx = hashmap->hash_fn(key, hashmap->key_size) & (hashmap->capacity - 1);
+  for (size_t i = idx; i < hashmap->capacity; i++) {
+    if (hashmap->states[i] == 0) {
+      memcpy((char *)hashmap->keys + i * hashmap->key_size, key, hashmap->key_size);
+      memcpy((char *)hashmap->values + i * hashmap->value_size, value, hashmap->value_size);
+      hashmap->states[i] = 1;
+      break;
+    }
+  }
+}
+
+bool _hashmap_get(hashmap_t *hashmap, void *key, void *out_target) {
+  size_t idx = hashmap->hash_fn(key, hashmap->key_size) & (hashmap->capacity - 1);
+
+  for (size_t i = idx; i < hashmap->capacity; i++) {
+    char state = hashmap->states[i];
+    if (state == 0)
+      return false;
+
+    if (state == 1) {
+      void *test_key = (char *)hashmap->keys + i * hashmap->key_size;
+      if (hashmap->cmp_fn(test_key, key, hashmap->key_size) == true) {
+        void *value = (char *)hashmap->values + i * hashmap->value_size;
+        memcpy(out_target, value, hashmap->value_size);
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
 
 #endif
 
