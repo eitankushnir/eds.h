2e4ff04eb84ca2cddbe79955ce5e968441bd3c4f
eitankushnir <etn121415@gmail.com>
2026-06-16 09:09:10 +0300

    WIP. new list type insteady of strechy buffer

--- eds.h
+++ eds.h
@@ -1,325 +1,153 @@
-#ifndef EDS_LIST_H
-#define EDS_LIST_H
-
-#define EDS_LIST_INITIAL_CAPACITY 10
-#define IS_POINTER_TO(a, b) _Generic((a), typeof((b)) *: 1, default: 0)
-#define ASSERT_POINTER(a, b) static_assert(IS_POINTER_TO((a), (b)), "Error: type mismatch")
-
-#define SAME_TYPE(a, b) _Generic((a), typeof((b)): 1, default: 0)
-#define ASSERT_SAME_TYPE(a, b) static_assert(SAME_TYPE((a), (b)), "Type mismatch")
-
-#define IS_DYNAMIC_ARRAY(a) _Generic((&a), typeof(*(a)) **: 1, default: 0)
-#define ASSERT_DYNAMIC_ARRAY(a) static_assert(IS_DYNAMIC_ARRAY((a)), "Cannot append to static array")
-
-#define IS_STRING_ARRAY(a) _Generic((&a), char (*)[]: 1, default: 0)
-#define ASSERT_STR_ARR(a) static_assert(IS_STRING_ARRAY((a)), "NOT STRING ARRAY")
-
-#define IS_FREE_FUNC(a) _Generic((a), void (*)(void *): 1, default: 0)
-#define ASSERT_FREE_FUNC(a) static_assert(IS_FREE_FUNC((a)), "Error: Function cannot be used as a free function. Signature mismatch")
+#ifndef EDS__H
+#define EDS__H
 #include <stddef.h>
-
-#define elem_size(list) sizeof(typeof(*(list)))
-
-#define list_append(list, value)                             \
-  do {                                                       \
-    ASSERT_DYNAMIC_ARRAY(list);                              \
-    typeof_unqual((value)) lvalue = (value);                 \
-    typeof_unqual(*(list)) type_check;                       \
-    ASSERT_SAME_TYPE(type_check, lvalue);                    \
-    _list_append((void **)&(list), &lvalue, sizeof(lvalue)); \
-  } while (0)
-
-#define list_size(list) _list_size((list))
-#define list_capacity(list) _list_capacity((list))
-#define list_destroy(list) \
-  _list_destroy((void **)&(list), sizeof(typeof(*list)))
-
-#define list_set_free_fn(list, free_fn) \
-  _list_set_free_fn((void **)&(list), free_fn, sizeof(typeof(*list)))
-
-#define list_free_fn(list, item) _list_free_item(list, item);
-
-#define list_isempty(list) _list_size((list)) == 0
-
-#define list_foreach(list, item)                     \
-  for (size_t EDS_ITERATOR = 0, EDS_KEEP = 1;        \
-       EDS_KEEP && EDS_ITERATOR < list_size((list)); \
-       EDS_KEEP = !EDS_KEEP, EDS_ITERATOR++)         \
-    for (typeof(*(list)) item = (list)[EDS_ITERATOR]; EDS_KEEP; EDS_KEEP = !EDS_KEEP)
-
-#define list_reserve(list, capacity) \
-  _list_reserve((void **)&(list), capacity, sizeof(typeof(*list)))
-
-#define list_trim(list) \
-  _list_trim((void **)&(list), sizeof(typeof(*list)))
-
-#define list_clear(list) \
-  _list_clear((list), sizeof(typeof(*list)))
-
-#define list_insert(list, i, value)                             \
-  do {                                                          \
-    ASSERT_DYNAMIC_ARRAY(list);                                 \
-    typeof_unqual((value)) lvalue = (value);                    \
-    typeof_unqual(*(list)) type_check;                          \
-    ASSERT_SAME_TYPE(type_check, lvalue);                       \
-    _list_insert((void **)&(list), i, &lvalue, sizeof(lvalue)); \
-  } while (0)
-
-#define list_pop(list, i, target) \
-  _list_pop((list), i, elem_size(list), (target))
-
-#define list_remove(list, i) \
-  _list_remove((list), i, sizeof(typeof(*list)));
-
-#define list_last(list) \
-  (list)[_list_size((list)) - 1]
-
-#define list_copy(list) \
-  _list_copy(list, elem_size(list))
-
-struct list_header {
-  size_t size;
-  size_t capacity;
-  void (*free_fn)(void *); // Function to free complex data types.
-};
-
-struct list_header *_list_header(void *lst);
-size_t _list_capacity(void *lst);
-size_t _list_size(void *lst);
-void (*_list_free_fn(void *lst))(void *);
-void _list_free_item(void *lst, void *item);
-
-void _list_set_capacity(void *lst, size_t capacity);
-void _list_set_size(void *lst, size_t size);
-void _list_set_free_fn(void **lp, void (*free_fn)(void *), size_t ds);
-
-void *_list_grow(void *lst, size_t n, size_t ds);
-
-void _list_reserve(void **lp, size_t n, size_t ds);
-
-void _list_append(void **lp, void *data, size_t ds);
-void _list_destroy(void **lp, size_t ds);
-
-void _list_trim(void **lp, size_t ds);
-void _list_clear(void *lst, size_t ds);
-
-void _list_insert(void **lp, size_t i, void *data, size_t ds);
-void _list_remove(void *lst, size_t i, size_t ds);
-void _list_pop(void *lst, size_t i, size_t ds, void *out);
-
-void *_list_copy(void *lst, size_t ds);
-
-#endif
-
-#ifdef EDS_IMPLEMENTATION
-
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct list_header *_list_header(void *lst) {
-  struct list_header *header = (struct list_header *)lst - 1;
-  return header;
-}
+#ifndef EDS_NO_LIST
+#define EDS_LIST_INITIAL_CAPCITY 10
 
-size_t _list_capacity(void *lst) {
-  if (!lst)
-    return 0;
+typedef struct list list_t;
 
-  return _list_header(lst)->capacity;
-}
+#define list(type) _list_create(EDS_LIST_INITIAL_CAPCITY, sizeof(type), NULL)
+#define list_with_capcity(type, capacity) _list_create((capacity), sizeof((type)), NULL)
+#define list_with_free(type, free_fn) _list_create(EDS_LIST_INITIAL_CAPCITY, sizeof((type)), (free_fn))
+#define list_with_capacity_free(type, capacity, free_fn) _list_create((capacity), sizeof((type)), (free_fn))
 
-size_t _list_size(void *lst) {
-  if (!lst)
-    return 0;
+#define list_destroy(list) _list_destroy(&(list))
 
-  return _list_header(lst)->size;
-}
+#define list_at_as(list, index, type) (*(type *)list_at((list), (index)))
+#define list_set(list, index, data)      \
+  do {                                   \
+    typeof((data)) lvalue = data;        \
+    _list_set((list), (index), &lvalue); \
+  } while (0)
 
-void (*_list_free_fn(void *lst))(void *) {
-  if (!lst)
-    return NULL;
+#define list_append(list, data)    \
+  do {                             \
+    typeof((data)) lvalue = data;  \
+    _list_append((list), &lvalue); \
+  } while (0)
 
-  return _list_header(lst)->free_fn;
-}
+size_t list_size(list_t *list);
+size_t list_capacity(list_t *list);
+bool list_is_empty(list_t *list);
 
-void _list_free_item(void *lst, void *item) {
-  void (*free_fn)(void *) = _list_free_fn(lst);
-  if (free_fn)
-    free_fn(item);
-}
+void *list_at(list_t *list, size_t index);
+void _list_set(list_t *list, size_t index, void *data);
+void _list_append(list_t *list, void *data);
 
-void _list_set_capacity(void *lst, size_t capacity) {
-  _list_header(lst)->capacity = capacity;
-}
-void _list_set_size(void *lst, size_t size) {
-  _list_header(lst)->size = size;
-}
+list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *));
+void _list_destroy(list_t **list);
+#endif // EDS_NO_LIST
 
-void _list_set_free_fn(void **lp, void (*free_fn)(void *), size_t ds) {
-  void *lst = *lp;
-  if (!lst) {
-    lst = _list_grow(lst, EDS_LIST_INITIAL_CAPACITY, ds);
-    *lp = lst;
-  }
-
-  _list_header(lst)->free_fn = free_fn;
-}
-
-void *_list_grow(void *lst, size_t n, size_t ds) {
-  void *ptr;
-  size_t alloc = n * ds + sizeof(struct list_header);
-  if (!lst) {
-    ptr = malloc(alloc);
-    struct list_header *header = (struct list_header *)ptr;
-    header->size = 0;
-    header->capacity = n;
-    header->free_fn = NULL;
-  } else {
-    ptr = realloc(_list_header(lst), alloc);
-    struct list_header *header = (struct list_header *)ptr;
-    header->capacity = n;
-  }
-
-  void *new_lst = (struct list_header *)ptr + 1;
-  return new_lst;
-}
-
-void _list_append(void **lp, void *data, size_t ds) {
-  void *lst = *lp;
-  size_t cap = _list_capacity(lst);
-  size_t size = _list_size(lst);
-
-  if (size >= cap) {
-    size_t new_cap = cap == 0 ? EDS_LIST_INITIAL_CAPACITY : cap * 2;
-    lst = _list_grow(lst, new_cap, ds);
-    *lp = lst;
-  }
-
-  char *new_elem_ptr = (char *)lst + ds * size;
-  memcpy(new_elem_ptr, data, ds);
-  _list_set_size(lst, size + 1);
-}
-
-void _list_destroy(void **lp, size_t ds) {
-  void *lst = *lp;
-  if (!lst)
-    return;
-
-  struct list_header *header = _list_header(lst);
-  size_t size = header->size;
-  void (*free_fn)(void *) = header->free_fn;
-
-  if (free_fn) {
-    char *lst_char = (char *)lst;
-    for (size_t i = 0; i < size; i++) {
-      void *item = *(void **)(lst_char + i * ds);
-      free_fn(item);
-    }
-  }
-
-  free(header);
-  *lp = NULL;
-}
-
-void _list_reserve(void **lp, size_t n, size_t ds) {
-  if (!n || !ds)
-    *lp = NULL;
-
-  void *lst = *lp;
-  *lp = _list_grow(lst, n, ds);
-}
-
-void _list_trim(void **lp, size_t ds) {
-  void *lst = *lp;
-  if (!lst)
-    return;
-
-  if (_list_size(lst) == 0) {
-    _list_destroy(lp, ds);
-    return;
-  }
-
-  *lp = _list_grow(lst, _list_size(lst), ds);
-}
-
-void _list_clear(void *lst, size_t ds) {
-  if (!lst)
-    return;
-
-  struct list_header *header = _list_header(lst);
-  if (header->free_fn) {
-    char *lst_char = (char *)lst;
-    for (size_t i = 0; i < _list_size(lst); i++) {
-      void *item = *(void **)(lst_char + i * ds);
-      header->free_fn(item);
-    }
-  }
-
-  header->size = 0;
-}
-
-void _list_insert(void **lp, size_t i, void *data, size_t ds) {
-  void *lst = *lp;
-  size_t cap = _list_capacity(lst);
-  size_t size = _list_size(lst);
-
-  if (size >= cap) {
-    size_t new_cap = cap == 0 ? EDS_LIST_INITIAL_CAPACITY : cap * 2;
-    lst = _list_grow(lst, new_cap, ds);
-    *lp = lst;
-  }
-
-  char *lst_char = (char *)lst;
-  for (size_t j = size; j > i; j--) {
-    memcpy(lst_char + ds * j, lst_char + ds * (j - 1), ds);
-  }
-
-  char *new_elem_ptr = lst_char + ds * i;
-  memcpy(new_elem_ptr, data, ds);
-  _list_set_size(lst, size + 1);
-}
-
-void _list_remove(void *lst, size_t i, size_t ds) {
-  size_t size = _list_size(lst);
-
-  char *lst_char = (char *)lst;
-  struct list_header *header = _list_header(lst);
-  if (header->free_fn) {
-    free(*(void **)(lst_char + ds * i));
-  }
-
-  for (size_t j = i; j < size - 1; j++) {
-    memcpy(lst_char + ds * j, lst_char + ds * (j + 1), ds);
-  }
-
-  _list_set_size(lst, size - 1);
-}
-
-void _list_pop(void *lst, size_t i, size_t ds, void *out) {
-  size_t size = _list_size(lst);
-
-  char *lst_char = (char *)lst;
-  memcpy(out, lst_char + ds * i, ds);
-
-  for (size_t j = i; j < size - 1; j++) {
-    memcpy(lst_char + ds * j, lst_char + ds * (j + 1), ds);
-  }
-
-  _list_set_size(lst, size - 1);
-}
-
-void *_list_copy(void *lst, size_t ds) {
-  if (!lst)
-    return NULL;
-
-  struct list_header *header = _list_header(lst);
-  size_t total_alloc = header->capacity * ds + sizeof(struct list_header);
-  void *copy = malloc(total_alloc);
-  memcpy(copy, header, total_alloc);
-
-  struct list_header *copy_header = copy;
-  copy_header->free_fn = NULL;
-  return (void *)(copy_header + 1);
-}
 #endif
+
+#define EDS_IMPLEMENTATION
+#ifdef EDS_IMPLEMENTATION
+
+#include <stdarg.h>
+
+void eds_error(char *format, ...) {
+  va_list vargs;
+  va_start(vargs, format);
+  fprintf(stderr, "EDS ERROR: ");
+  vfprintf(stderr, format, vargs);
+  fprintf(stderr, "\n");
+  va_end(vargs);
+  exit(128);
+}
+
+void *eds_malloc(size_t bytes) {
+  void *p = malloc(bytes);
+  if (!p)
+    eds_error("Failed to allocate %zu bytes.", bytes);
+  return p;
+}
+
+void *eds_realloc(void *p, size_t bytes) {
+  p = realloc(p, bytes);
+  if (!p)
+    eds_error("Failed to reallocate %zu bytes.", bytes);
+  return p;
+}
+
+#ifndef EDS_NO_LIST
+
+struct list {
+  void *data;
+  size_t size;
+  size_t capacity;
+  size_t data_size;
+
+  bool is_sotring_pointers;
+
+  void (*free_fn)(void *);
+};
+
+list_t *_list_create(size_t capacity, size_t data_size, void (*free_fn)(void *)) {
+  if (!capacity) {
+    eds_error("list_t cannot have 0 capacity");
+  }
+  list_t *list = eds_malloc(sizeof(list_t));
+  list->capacity = capacity;
+  list->size = 0;
+  list->data_size = data_size;
+  list->free_fn = free_fn;
+  list->data = eds_malloc(data_size * capacity);
+  return list;
+}
+
+void _list_destroy(list_t **listptr) {
+  list_t *l = *listptr;
+  if (!l)
+    return;
+
+  if (l->free_fn) {
+    for (size_t i = 0; i < l->size; i++) {
+    }
+  }
+
+  free(l->data);
+  free(l);
+
+  *listptr = NULL;
+}
+
+size_t list_size(list_t *list) {
+  return list->size;
+}
+size_t list_capacity(list_t *list) {
+  return list->capacity;
+}
+bool list_is_empty(list_t *list) {
+  return list->size == 0;
+}
+
+void *list_at(list_t *list, size_t index) {
+  if (index >= list->size)
+    eds_error("Index %zu is out of bounds in list of size %zu.", index, list->size);
+
+  return (char *)list->data + list->data_size * index;
+}
+
+void _list_set(list_t *list, size_t index, void *data) {
+  if (index >= list->size)
+    eds_error("Index %zu is out of bounds in list of size %zu.", index, list->size);
+
+  memcpy((char *)list->data + index * list->data_size, data, list->data_size);
+}
+
+void _list_append(list_t *list, void *data) {
+  if (list->size >= list->capacity) {
+    list->data = eds_realloc(list->data, list->data_size * list->capacity * 2);
+    list->capacity *= 2;
+  }
+
+  memcpy((char *)list->data + list->size * list->data_size, data, list->data_size);
+  list->size++;
+}
+#endif // EDS_NO_LIST
+
+#endif // EDS_IMPLEMENTATION
